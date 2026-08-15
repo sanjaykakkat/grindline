acbfb0bc98ef4e4862b66acea01526e380788109
Sanjay <sungodnikaisme@gmail.com>
2026-08-15 21:10:47 +0530

    Refactor signup to streamline user document creation
    Refactor signup function to use createUserDocument for user data creation.

--- auth.js
+++ auth.js
@@ -43,16 +43,22 @@ function getDefaultUserData(name, birthDate, email) {
   };
 }
 
+// ----- Create user document (can also be used as fallback) -----
+export async function createUserDocument(uid, name, birthDate, email) {
+  const userData = getDefaultUserData(name, birthDate, email);
+  await setDoc(doc(db, "users", uid), userData);
+  return userData;
+}
+
 // ----- Signup -----
-export async function signup(email, password, name, birthDate, gender) {
+export async function signup(email, password, name, birthDate) {
   const cred = await createUserWithEmailAndPassword(auth, email, password);
 
   // Set displayName
   await updateProfile(cred.user, { displayName: name });
 
   // Create user document in Firestore
-  const userData = getDefaultUserData(name, birthDate, email, gender);
-  await setDoc(doc(db, "users", cred.user.uid), userData);
+  await createUserDocument(cred.user.uid, name, birthDate, email);
 
   return cred.user;
 }
