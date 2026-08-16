bfc072c34c928f56462fea4e92322ae1b03289a6
Sanjay <sungodnikaisme@gmail.com>
2026-08-16 20:47:00 +0530

    Add service worker for handling notifications

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+self.addEventListener("install", () => {
+  console.log("Grindline Service Worker installed.");
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+
+  event.waitUntil(
+    clients.matchAll({
+      type: "window",
+      includeUncontrolled: true
+    }).then((clientList) => {
+      for (const client of clientList) {
+        if ("focus" in client) {
+          return client.focus();
+        }
+      }
+
+      if (clients.openWindow) {
+        return clients.openWindow(
+          "https://sanjaykakkat.github.io/grindline/"
+        );
+      }
+    })
+  );
+});
