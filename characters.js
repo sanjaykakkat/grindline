b470f4607026c606c1828293ca6e94a574b991d5
Sanjay <sungodnikaisme@gmail.com>
2026-08-28 09:44:52 +0530

    Add variations to Luffy and Nami characters

--- characters.js
+++ characters.js
@@ -10,27 +10,33 @@ export const characters = {
   luffy_001: {
     id: "luffy_001",
     name: "Luffy",
+    variation: "Fake Luffy",
     gender: "male",
     levelRequired: 1,
     tier: "fake",
+    price: 0,
     image: "https://res.cloudinary.com/wbr5igfj/image/upload/v1786824091/1000064964.png"
   },
 
   luffy_002: {
     id: "luffy_002",
     name: "Luffy",
+    variation: "Fusia Village",
     gender: "male",
     levelRequired: 5,
     tier: "rookie",
+    price: 250000,
     image: "https://res.cloudinary.com/wbr5igfj/image/upload/v1786891659/1786891604358.png"
   },
 
   luffy_003: {
     id: "luffy_003",
     name: "Luffy",
+    variation: "East Blue",
     gender: "male",
     levelRequired: 10,
     tier: "strong",
+    price: 500000,
     image: "https://res.cloudinary.com/wbr5igfj/image/upload/v1787713189/IMG_20260826_081718.png"
   },
 
@@ -42,27 +48,33 @@ export const characters = {
   nami_001: {
     id: "nami_001",
     name: "Nami",
+    variation: "Fake Nami",
     gender: "female",
     levelRequired: 1,
     tier: "fake",
+    price: 0,
     image: "https://res.cloudinary.com/wbr5igfj/image/upload/v1786889869/1786889838249.png"
   },
 
   nami_002: {
     id: "nami_002",
     name: "Nami",
+    variation: "Fusia Village",
     gender: "female",
     levelRequired: 5,
     tier: "rookie",
+    price: 250000,
     image: "https://res.cloudinary.com/wbr5igfj/image/upload/v1787299298/1787299193823.png"
   },
 
   nami_003: {
     id: "nami_003",
     name: "Nami",
+    variation: "East blue",
     gender: "female",
     levelRequired: 10,
     tier: "strong",
+    price: 500000,
     image: "./images/characters/nami_003.png"
   },
 
